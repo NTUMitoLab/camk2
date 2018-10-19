ff2fcc695a7d9ad63a7cf39cac68b1a0da69fe74
Tseng Wen-Wei <sosiristseng@gmail.com>
2018-10-19 08:17:31 +0800

    camkii juloia

--- camkii.jl
+++ camkii.jl
@@ -7,10 +7,10 @@ DOI: 10.1529/biophysj.107.118505, PMID: 18502812, PMCID: PMC2517018
 =#
 
 
-using DifferentialEquations
-using Parameters
+using DifferentialEquations, Parameters, LabelledArrays
 
 #=
+Default params
 TOT_CAM = 6e-3
 K1 = 2.5
 Km1 = 0.05
@@ -38,16 +38,21 @@ _mm(x, k) = x / (x + k)
 
 # δ Isoform by default (All concentrations in mM and time in ms)
 @with_kw_noshow struct CAMKIIParams
+    # concentrations
     TOT_CAM = 6.0e-3
     TOT_CAMKII = 0.1e-3
-    K1 = 2.5
-    Km1 = 0.05
-    K2 = 88.25
-    Km2 = 0.05
-    K3 = 12.5
-    Km3 = 1.25
-    K4 = 250
-    Km4 = 1.25
+    ATP = 1.0
+    PP1 = 1e-6
+    # CaM association and idssociation constant
+    K_P1 = 2.5
+    K_M1 = 0.05
+    K_P2 = 88.25
+    K_M2 = 0.05
+    K_P3 = 12.5
+    K_M3 = 1.25
+    K_P4 = 250
+    K_M4 = 1.25
+    # CaMKII association and idssociation constant
     K_ASSO = 2.1
     K_DISSO = 0.7E-4
     K_DISSO_2 = 1E-3 * K_DISSO
@@ -56,26 +61,52 @@ _mm(x, k) = x / (x + k)
     KM_CAM = 3E-5
     KCAT = 5.4E-3
     KM_ATP = 19.1E-3
-    ATP = 1.0
+
     ATP_FAC = _hills(ATP, KM_ATP, 1)
     KCAT_PP1 = 1.72E-3
     KM_PP1 = 11E-3
     PP1 = 1e-6
-    CA_PULSE = false
+    # Calcium Pulse parameters
+    INTERVAL = 0
     KP = 12e-6
     KM_PUMP = 0.1e-3
     QPUMP_REST = 40e-6
     QM = 60E-6
     TP = 25.0
-    INTERVAL = 1000.0
+    # CaMKII target paramaters
     KCAT_TARGET = 1E-3
-    MAX_INHIBITION = 0.1
-    KD_TARGET = KCAT_TARGET * MAX_INHIBITION / (1 - MAX_INHIBITION)
+    MAX_SS_INHIB = 0.1
+    KD_TARGET = KCAT_TARGET * MAX_SS_INHIB / (1 - MAX_SS_INHIB)
 end
 
-labels = Dict(:CaMCa1 => 1, :CaMCa2 => 2, :CaMCa3 => 3, :CaMCa4 => 4,
-              :Ca => 8, :CaMKII4 => 5, :CaMKIIP=>6 , :CaMKIIP4 => 7, :target=>9)
+u0 = @LVector zeros(8) (:ca, :camca1, :camca2, :camca3, :camca4, :camk2_camca4, :camk2_p, :camk2_p_camca4)
 
+function pulse_ca!(du, u, p, t)
+    @unpack KP, KM_PUMP, QM, TP, QPUMP_REST, INTERVAL = p
+    if INTERVAL > 0
+        localTime = rem(t, INTERVAL)
+        ca = u.ca
+        q_pump = KP * _hills(ca, KM_PUMP, 2)
+        q_rel = QM * (localTime/TP * exp(1 - localTime/TP))^4 + QPUMP_REST
+        du.ca = q_rel - q_pump
+    else
+        du.ca = 0.0
+    end
+    return du
+end
+
+function cam_system!(du, u, p, t)
+    @unpack TOT_CAM, KP1, KM1, KP2, KM2, KP3, KM3, KP4, KM4 = p
+    cam = TOT_CAM - (u.camca1 + u.camca2 + u.camca3 + u.camca4)
+    v1 = u.ca * cam * KP1 - u.camca1 * KM1
+    v2 = u.ca * u.camca1 * KP2 - u.camca2 * KM2
+    v1 = u.ca * u.camca1 * KP3 - u.camca3 * KM3
+    v1 = u.ca * u.camca1 * KP4 - u.camca4 * KM4
+end
+
+function camkk2_system!(du, u, p, t)
+
+end
 
 function cam!(du, u, p, t)
     # Rate of CaM-Ca association
